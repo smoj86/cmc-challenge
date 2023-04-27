c03eb58a051111a56936bc4cd55ccd28c0f1b9be
smoj86 <armaghanmehran@Armaghans-MacBook-Air.local>
2023-04-28 04:09:29 +1000

    adding new files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'cmc-background': '#1b2429',
+        'cmc-text': '#FFF',
+        'cmc-blue': '#00deff'
+      }
+    },
+  },
+  plugins: [],
+}
+
